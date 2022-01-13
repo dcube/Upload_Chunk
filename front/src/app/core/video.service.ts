4dce1c0d014a25fabef69e055e2707588d969c40
Nicolas Bailly <Nicolas.Bailly@dcube.fr>
2022-01-13 13:28:46 +0100

    Front : Suppression du FileReader + mise en place de promise

--- front/src/app/core/video.service.ts
+++ front/src/app/core/video.service.ts
@@ -1,94 +1,87 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Guid } from 'guid-typescript';
-import { throwError, Observable, forkJoin, Subscription } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { throwError, Observable, from, mergeAll, of } from 'rxjs';
+import { tap, retryWhen, concatMap, delay } from 'rxjs/operators';
 import { Result } from './result';
+
 
 @Injectable({
     providedIn: 'root'
   })
   export class VideoService {
     private baseUrl = `http://localhost:7071/api`;
-    private chunkSize = 1048576;//1Mo
-    private filesChunk = new Subscription();
+    private chunkSize = 5242880;//5Mo
   
     constructor(private httpClient: HttpClient) {}
 
-    uploadFile(file: File, yieldResultFunc: (result: Result | null, total: number, error: any) => void) {
-        const id = Guid.create().toString();
-        const reader = new FileReader();
-        const fileName = file.name;
-    
-        reader.onload = (e: ProgressEvent) =>
-          // after reading, start sending data
-          this.sendBufferData(reader.result, id, fileName, yieldResultFunc);
-    
-        // read file data as ArrayBuffer
-        reader.readAsArrayBuffer(file);
-      }
+    getTotalChunks(fileSize: number){
+      return Math.ceil(fileSize / this.chunkSize);
+    }
 
-      private async sendBufferData(
-        readResult: string | ArrayBuffer | null,
-        id: string,
-        fileName: string,
-        yieldResultFunc: (result: Result | null, total: number, error: any) => void
-      ) {
-        let buffer = new Uint8Array(readResult as ArrayBuffer);
-        this.filesChunk = forkJoin(this.getChunkRequests(buffer, id, yieldResultFunc)).subscribe((result) => {
-          let hasFailedChunkFiles: Result | undefined = result.find((res: Result) => res.isSuccess === false);
+    uploadFile(id: Guid, file: File){
+      const observables = this.getChunkRequests(id, file);
+
+      return from(observables)
+      .pipe(mergeAll(5));
+    }
+
+    finalize(id: Guid, resultList: Result[], fileName: string){
+      let hasFailedChunkFiles: Result | undefined = resultList.find((res: Result) => res.isSuccess === false);
     
-          if (!hasFailedChunkFiles) {
-            const url = `${this.baseUrl}/files/finalize?fileid=${id}&fileName=${fileName}`;
-            const params = new HttpParams({});
-    
-            this.httpClient
-              .post<Result>(url, { params })
-              .pipe(
-                catchError((err) => {
-                  err.message = `Erreur lors de la finalisation de l'upload du fichier ${id}`;
-                  return throwError(err);
-                })
-              )
-              .subscribe();
-          }
+      if (!hasFailedChunkFiles) {
+        const url = `${this.baseUrl}/files/finalize?fileid=${id.toString()}&fileName=${fileName}`;
+        const params = new HttpParams({});
+
+        return this.httpClient
+          .post<Result>(url, { params });
+      }
+      return throwError(() => 'Certains chunks ont échoué');
+    }
+
+    private getChunkRequests(id: Guid, file: File) {
+      console.log('getChunkRequests');
+
+      const requestArray = new Array<Observable<Result>>();
+      const url = `${this.baseUrl}/files/chunk`;
+
+      // Number of parts (exclusive last part!)
+      var chunks = this.getTotalChunks(file.size);
+      // Iterate the parts
+      for (var i = 0; i < chunks; i++) {
+        var start = i * this.chunkSize;
+
+        const formData = new FormData();
+        formData.append('file', file.slice(start, this.chunkSize));
+
+        const params = new HttpParams({
+          fromString: `fileid=${id.toString()}&index=${i.toString()}`
         });
+        const request = this.httpClient
+          .post<Result>(url, formData, { params }).pipe(tap(
+            (result)=>{
+            return result;
+          },
+          (error) => {
+            console.log(error);
+            return error;
+          }),
+          retryWhen(errors => errors
+            .pipe(
+              concatMap((error, count) => {
+                if (count < 5 && (error.status == 400 || error.status == 0)) {
+                    return of(error.status);
+                }
+                console.log('retry : ' + error);
+                return throwError(() =>error);
+              }),
+              delay(1000)
+            )));
+
+        requestArray.push(request);
       }
 
-      private getChunkRequests(buffer: Uint8Array, id: string, yieldResultFunc: (result: Result | null, total: number, error: any) => void) {
-        const url = `${this.baseUrl}/files/chunk`;
-        let fileIndex = 0;
-        const requestArray = new Array<Observable<Result>>();
-        const total = Math.round(buffer.length / this.chunkSize);
-        for (let i = 0; i < buffer.length; i += this.chunkSize) {
-          let indexTo = i + this.chunkSize;
-          if (indexTo >= buffer.length) {
-            indexTo = buffer.length - 1; // for last data.
-          }
-          const formData = new FormData();
-          formData.append('file', new Blob([buffer.subarray(i, indexTo)]));
-          const params = new HttpParams({
-            fromString: `fileid=${id}&index=${fileIndex.toString()}`
-          });
-          const request = this.httpClient
-            .post<Result>(url, formData, { params })
-            .pipe(
-              tap(
-                (result) => {
-                  return yieldResultFunc(result, total, null);
-                },
-                (error) => {
-                  return yieldResultFunc(null, total, error);
-                }
-              )
-            );
-          requestArray.push(request);
-          fileIndex += 1;
-        }
-        return requestArray;
-      }
-    
-      stopUploadVideo() {
-        this.filesChunk.unsubscribe();
-      }
+      return requestArray;
+      
+    }
   }
--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FileInput } from 'ngx-material-file-input';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Guid } from 'guid-typescript';
 import { VideoService } from 'src/app/core/video.service';
+import { Result } from 'src/app/core/result';
+import { Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +13,13 @@ import { VideoService } from 'src/app/core/video.service';
 })
 export class HomeComponent implements OnInit {
   form!: FormGroup;
-  uploadFile!: File;
+  uploadFile?: File;
   isLoading: boolean = false;
   finishUploadVideo: boolean = false;
   uploadProgress = 0;
+  chunkRequestApi = new Subscription();
 
-  constructor(private formBuilder: FormBuilder, private videoService: VideoService,) { }
+  constructor(private formBuilder: FormBuilder, private videoService: VideoService) { }
 
 
   ngOnInit(): void {
@@ -25,45 +29,62 @@ export class HomeComponent implements OnInit {
     })
 
     this.form.controls['video'].valueChanges.subscribe((fileInput: FileInput) => {
-      this.uploadFile = fileInput.files[0];
+      if (fileInput !== null && fileInput.files.length > 0){
+        this.uploadFile = fileInput.files[0];
+      } else {
+        this.uploadFile = undefined;
+      }
     });
   }
 
+  //Dans cette méthode, mettre tout ce qui concerne l'annulation de l'upload
   cancelUpload() {
+    this.chunkRequestApi.unsubscribe()
     this.isLoading = false;
     this.uploadProgress = 0;
   }
 
   uploadVideo() {
-    if (this.uploadFile == null) {
+    if (this.uploadFile === undefined) {
       return;
     }
 
     this.isLoading = true;
 
-    let index = 1;
-    this.videoService.uploadFile(this.uploadFile, (result, total, error) => {
-      if (error) {
-        console.log(error.message);
-      }
+    const id = Guid.create();//Creation de l'identifiant du fichier
+    let nbUploadeChunck = 1;//Nombre de chunck qui ont été envoyé
+    let total = this.videoService.getTotalChunks(this.uploadFile!.size);//Nombre total de chunks à envoyer
+    let resultList: Result[] = [];//On enregistre tous les résultats d'upload pour savoir s'il y a eu des erreurs
+    this.chunkRequestApi = this.videoService.uploadFile(id, this.uploadFile!)
+    .subscribe({
+      next: (result) =>{
+        this.uploadProgress = Math.round((nbUploadeChunck / total) * 100);
+        resultList.push(result);
 
-      this.uploadProgress = Math.round((index / total) * 100);
+        //S'il y a une erreur, on arrete l'envoie
+        if (result != null && result.isSuccess === false) {
+          this.cancelUpload();
 
-      if (result != null && result.isSuccess === false) {
-        this.cancelUpload();
-
-        if (result.message) {
-          console.log(result.message);
+          if (result.message) {
+            console.log(result.message);
+          }
         }
-      }
-
-      index++;
-
-      if (index > total) {
-        this.finishUploadVideo = true;
-        console.log('La vidéo a été correctement ajouté');
-      }
+        nbUploadeChunck++;
+        if (nbUploadeChunck > total) {
+          this.finishUploadVideo = true;
+          console.log('La vidéo a été correctement ajouté');
+          this.videoService.finalize(id, resultList, this.uploadFile!.name).subscribe({
+            next: (result) => {
+              console.log('Finalize terminé');
+              this.isLoading = false;
+            },
+            error: (e) => console.log(e.message)
+          })
+        }
+      },
+      error: (e) => console.log(e.message)
     });
+      
   }
 
 }
